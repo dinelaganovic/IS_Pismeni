68254e1aa9a44cde9ac00b05bbe3df5727aa2c86
dinela <dinelaganovic99@gmail.com>
2023-08-21 15:27:44 +0200

    day 2

--- ispismeni/src/app/app.module.ts
+++ ispismeni/src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PipePipe } from './pipe.pipe';
+import { HttpClientModule } from '@angular/common/http';
+import { TemplatedrivenformComponent } from './templatedrivenform/templatedrivenform.component';
+import { ReactiveformComponent } from './reactiveform/reactiveform.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PipePipe,
+    TemplatedrivenformComponent,
+    ReactiveformComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ ispismeni/src/app/interface.ts
@@ -0,0 +1,5 @@
+export interface Interface {
+    id:number,
+    email:string,
+    password:string
+}
