df957887a284b1490150bf171807f4ad23c3ed56
dinela <dinelaganovic99@gmail.com>
2023-09-08 10:50:02 +0200

    spb

---
+++ Septembar/Septembar/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Septembar.Context
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base()
+        {
+            
+        }
+        //dbset
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
