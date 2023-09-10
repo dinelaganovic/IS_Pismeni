030dda6f6f6df7b1492af9595339a200a58d4e9e
dinela <dinelaganovic99@gmail.com>
2023-09-10 16:56:30 +0200

    krajrokadostaodmene

--- Septembar/Septembar/Controllers/VaspitacicaController.cs
+++ Septembar/Septembar/Controllers/VaspitacicaController.cs
@@ -25,5 +25,104 @@ namespace Septembar.Controllers
                 Message = "Uspešno ste se prijavili!"
             });
         }
+        [HttpPost("sacuvajdete")]
+        public async Task<IActionResult> CreateChild([FromBody] Child child)
+        {
+            appDbContext.children.Add(child);
+            await appDbContext.SaveChangesAsync();
+           return BadRequest();
+        }
+
+
+    // Metoda za dobijanje svih vaspitačica
+    [HttpGet]
+    public async Task<IActionResult> GetVaspitacice()
+    {
+        var vaspitacice = await appDbContext.vaspitacice.ToListAsync();
+        return Ok(vaspitacice);
     }
+
+    // Metoda za dobijanje vaspitačice po ID
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetVaspitacica(int id)
+    {
+        var vaspitacica = await appDbContext.vaspitacice.FirstOrDefaultAsync(v => v.Id == id);
+
+        if (vaspitacica == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(vaspitacica);
+    }
+
+    // Metoda za kreiranje vaspitačice
+    [HttpPost]
+    public async Task<IActionResult> CreateVaspitacica([FromBody] Vaspitacica vaspitacica)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                appDbContext.vaspitacice.Add(vaspitacica);
+                await appDbContext.SaveChangesAsync();
+                return Ok(vaspitacica);
+            }
+            return BadRequest(ModelState);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex}");
+        }
+    }
+
+    // Metoda za ažuriranje vaspitačice
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateVaspitacica(int id, [FromBody] Vaspitacica vaspitacica)
+    {
+        try
+        {
+            if (ModelState.IsValid)
+            {
+                if (id != vaspitacica.Id)
+                {
+                    return BadRequest("ID-jevi se ne podudaraju.");
+                }
+
+                appDbContext.Entry(vaspitacica).State = EntityState.Modified;
+                await appDbContext.SaveChangesAsync();
+                return Ok(vaspitacica);
+            }
+            return BadRequest(ModelState);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex}");
+        }
+    }
+
+    // Metoda za brisanje vaspitačice
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteVaspitacica(int id)
+    {
+        try
+        {
+            var vaspitacica = await appDbContext.vaspitacice.FirstOrDefaultAsync(v => v.Id == id);
+
+            if (vaspitacica == null)
+            {
+                return NotFound();
+            }
+
+            appDbContext.vaspitacice.Remove(vaspitacica);
+            await appDbContext.SaveChangesAsync();
+            return Ok(vaspitacica);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Internal server error: {ex}");
+        }
+    }
+
+}
 }
